022da0156c1c4a1618d46e3a9ab5206bcc060367
Seif Sarofim <seif.sarofim@cognitev.com>
2018-07-22 13:17:44 +0200

    removed /eventful prefix from endpoints

--- tests/test_views.py
+++ tests/test_views.py
@@ -17,17 +17,17 @@ class TestEventsEndpoint(TestCase):
 
     def test_gets_all_events(self):
         Event.objects.create(event_id="HELLO", retry_policy='{"max_retries": 3}')
-        response = self.client.get('/eventful/events')
+        response = self.client.get('/events')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content), json.loads('{"events": ["HELLO"]}'))
 
     def test_empty_returns_empty(self):
-        response = self.client.get('/eventful/events')
+        response = self.client.get('/events')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(b'{"events": []}', response.content)
 
     def test_post_returns_error_405(self):
-        reponse = self.client.post('/eventful/events')
+        reponse = self.client.post('/events')
         self.assertEqual(reponse.status_code, 405)
 
 
@@ -38,30 +38,30 @@ class TestSubscriptionEndpoint(TestCase):
 
     def test_creates_subscription(self):
         Event.objects.create(event_id="HELLO", retry_policy='{"max_retries": 3}')
-        response = self.client.post('/eventful/subscribe', {'event_id': 'HELLO', 'webhook': 'http://gamal.com'})
+        response = self.client.post('/subscribe', {'event_id': 'HELLO', 'webhook': 'http://gamal.com'})
         resp_json = json.loads(response.content)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(resp_json["event_id"], "HELLO")
         self.assertEqual(resp_json["webhook"], "http://gamal.com")
 
     def test_get_request_returns_405(self):
-        reponse = self.client.get('/eventful/subscribe')
+        reponse = self.client.get('/subscribe')
         self.assertEqual(reponse.status_code, 405)
 
     def test_error_404_when_not_valid_event(self):
         Event.objects.create(event_id="HELLO", retry_policy='{"max_retries": 3}')
-        response = self.client.post('/eventful/subscribe', {'event_id': 'que?', 'webhook': 'http://gamal.com'})
+        response = self.client.post('/subscribe', {'event_id': 'que?', 'webhook': 'http://gamal.com'})
         self.assertEqual(response.status_code, 404)
 
     def test_duplicate_webhooks_not_allowed(self):
         new_event = Event.objects.create(event_id="HELLO", retry_policy='{"max_retries": 3}')
         Subscription.objects.create(event=new_event, webhook="http://gamal.com")
-        response = self.client.post('/eventful/subscribe', {'event_id': 'HELLO', 'webhook': 'http://gamal.com'})
+        response = self.client.post('/subscribe', {'event_id': 'HELLO', 'webhook': 'http://gamal.com'})
         self.assertEqual(response.status_code, 400)
 
     def test_error_400_missing_arguments(self):
         Event.objects.create(event_id="HELLO", retry_policy='{"max_retries": 3}')
-        response = self.client.post('/eventful/subscribe', {'event_id': "HELLO", 'webhook': ''})
-        response2 = self.client.post('/eventful/subscribe', {'event_id': "HELLO"})
+        response = self.client.post('/subscribe', {'event_id': "HELLO", 'webhook': ''})
+        response2 = self.client.post('/subscribe', {'event_id': "HELLO"})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response2.status_code, 400)
