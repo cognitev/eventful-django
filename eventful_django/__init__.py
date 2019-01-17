d73ae912381baf716c0393f141d495edccc08a46
Eslam-gbh <31445843+Eslam-gbh@users.noreply.github.com>
2019-01-17 16:03:21 +0200

    Pump package version (#6)

--- eventful_django/__init__.py
+++ eventful_django/__init__.py
@@ -4,6 +4,6 @@ Eventful Django.
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 default_app_config = 'eventful_django.apps.EventfulDjangoConfig'  # pylint: disable=invalid-name
