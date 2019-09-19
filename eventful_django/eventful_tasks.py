dda524e0c9584e1f8ca439049b4d54c403fe22a4
Kareem Emad <kareememad400@gmail.com>
2019-09-19 17:16:20 +0200

    add pubsub notify/model

--- eventful_django/eventful_tasks.py
+++ eventful_django/eventful_tasks.py
@@ -4,39 +4,25 @@ Celery task for eventful_django
 from __future__ import absolute_import, unicode_literals
 
 from os import environ
-
-import requests
 from celery import Celery
 
-CELERY_APP = Celery(
-    'eventful_tasks',
-    backend=environ.get('EVENTFUL_BROKER_BACKEND', 'amqp'),
-    broker=environ.get('EVENTFUL_BROKER_URL', 'amqp://localhost//'))
+CELERY_APP = Celery('eventful_tasks',
+                    backend=environ.get('EVENTFUL_BROKER_BACKEND', 'amqp'),
+                    broker=environ.get('EVENTFUL_BROKER_URL',
+                                       'amqp://localhost//'))
 
 
 @CELERY_APP.task()
-def notify(webhook, event, payload, headers):
+def notify(hook, event, payload, headers, subscription_model):
     """
-    notifies webhook by sending it POST request.
+    notifies webhook/topic by sending it POST request/message.
     playload sent by caller.
     func is celery task to allow async operation.
     :type webhook: string
     :type event: string
     :type payload: dict
     """
-    try:
-        response = requests.request(
-            'POST',
-            webhook,
-            json={
-                "event": event,
-                "payload": payload
-            },
-            headers=headers,
-        )
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as error:
-        print(error)
+    subscription_model.notify(hook, event, payload, headers)
 
 
 @CELERY_APP.task(bind=True)
--- eventful_django/models.py
+++ eventful_django/models.py
@@ -8,8 +8,12 @@ import json
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
-
+from os import environ
 from .eventful_tasks import notify
+import requests
+from google.cloud import pubsub_v1
+
+PROJECT_ID = environ.get('GOOGLE_PROJECT_ID', 'cogni-sandbox')
 
 
 @python_2_unicode_compatible
@@ -18,7 +22,8 @@ class Subscription(models.Model):
     Subscription represents a webhook to event assignment
     """
     webhook = models.URLField('Web Hook URL')
-    event = models.ForeignKey("eventful_django.Event", on_delete=models.CASCADE)
+    event = models.ForeignKey("eventful_django.Event",
+                              on_delete=models.CASCADE)
     headers = models.TextField('Request Headers', null=True)
 
     class Meta:
@@ -29,6 +34,66 @@ class Subscription(models.Model):
         Get a string representation of this model instance.
         """
         return self.webhook
+
+    def notify(self, webhook, event, payload, headers):
+        """
+        notifies webhook by sending it POST request.
+        playload sent by caller.
+        func is celery task to allow async operation.
+        :type webhook: string
+        :type event: string
+        :type payload: dict
+        """
+        try:
+            response = requests.request(
+                'POST',
+                webhook,
+                json={
+                    "event": event,
+                    "payload": payload
+                },
+                headers=headers,
+            )
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as error:
+            print(error)
+
+
+@python_2_unicode_compatible
+class SubscriptionPubSub(models.Model):
+    """
+    Subscription represents a topic to event assignment
+    """
+    topic = models.CharField(max_length=100)
+    event = models.ForeignKey("eventful_django.Event",
+                              on_delete=models.CASCADE)
+    headers = models.TextField('Request Headers', null=True)
+
+    class Meta:
+        unique_together = (("topic", "event"))
+
+    def __str__(self):
+        """
+        Get a string representation of this model instance.
+        """
+        return self.topic
+
+    def notify(self, topic, event, payload, headers):
+        """
+        notifies topics by publsihing on it.
+        playload sent by caller.
+        func is celery task to allow async operation.
+        :type topic: string
+        :type event: string
+        :type payload: dict
+        """
+        try:
+            publisher = pubsub_v1.PublisherClient()
+            topic_path = publisher.topic_path(PROJECT_ID, topic)
+            payload_string = json.dumps(payload).encode('utf-8')
+            publisher.publish(topic_path, data=payload_string)
+        except Exception as e:
+            print(e)
 
 
 class Event(models.Model):
@@ -59,7 +124,20 @@ class Event(models.Model):
             headers = eval(subscription.headers or '{}')
             try:
                 notify.apply_async(
-                    (subscription.webhook, self.event_id, payload, headers),
+                    (subscription.webhook, self.event_id, payload, headers,
+                     subscription),
+                    retry=True,
+                    retry_policy=json.loads(self.retry_policy),
+                )
+            except notify.OperationalError as notification_error:
+                print(notification_error)
+
+        for subscription in self.subscriptionpubsub_set.all():
+            headers = eval(subscription.headers or '{}')
+            try:
+                notify.apply_async(
+                    (subscription.topic, self.event_id, payload, headers,
+                     subscription),
                     retry=True,
                     retry_policy=json.loads(self.retry_policy),
                 )
