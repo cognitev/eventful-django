6496ff990a8f9a4fb0701d39810de9e76c2943f3
Alaa Nour <alaanourali@gmail.com>
2020-06-24 12:55:47 +0200

    INS-5092 : Secure cloud functions (#23)

--- eventful_django/models.py
+++ eventful_django/models.py
@@ -7,10 +7,12 @@ from __future__ import absolute_import, unicode_literals
 import json
 import logging
 from itertools import chain
+import google.auth.transport.requests
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
+from google.oauth2.service_account import IDTokenCredentials
 from .eventful_tasks import notify, notify_pubsub
 
 LOGGER = logging.getLogger(__name__)
@@ -24,6 +26,10 @@ class Subscription(models.Model):
     webhook = models.URLField('Web Hook URL')
     event = models.ForeignKey("eventful_django.Event", on_delete=models.CASCADE)
     headers = models.TextField('Request Headers', null=True)
+    is_cloud_function = models.BooleanField(default=False)
+    client_email = models.CharField(max_length=250, null=True, blank=True)
+    token_uri = models.CharField(max_length=250, null=True, blank=True)
+    private_key = models.TextField('Private Key', null=True)
 
     class Meta:
         unique_together = (("webhook", "event"))
@@ -34,11 +40,34 @@ class Subscription(models.Model):
         """
         return self.webhook
 
+    def generate_jwt_token(self, cloud_function_endpoint):
+        """
+        Generate Jwt token for this endpoint.
+        """
+        info = {
+            "private_key": self.private_key,
+            "client_email": self.client_email,
+            "token_uri": self.token_uri,
+        }
+        credentials = IDTokenCredentials.from_service_account_info(
+            info=info,
+            target_audience=cloud_function_endpoint,
+        )
+
+        request = google.auth.transport.requests.Request()
+        credentials.refresh(request)
+        return credentials.token
+
     def notify(self, event, payload, retry_policy):
         """
         Start Notify Job for specified url hook
         """
         headers = eval(self.headers or "{}")  # pylint: disable=eval-used
+
+        if self.is_cloud_function:
+            jwt_token = self.generate_jwt_token(self.webhook)
+            headers = {'Authorization': f'Bearer {jwt_token}', **headers}
+
         notify.apply_async(
             (self.webhook, event, payload, headers),
             retry=True,
